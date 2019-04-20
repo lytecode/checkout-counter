43219a6ac184cb1fc1b975977469ea93ac33f014
Basil <mbonubasil@gmail.com>
2019-04-20 11:15:24 +0100

    Updates

---
+++ precache-manifest.3c8ee2134c90e2e6185f80dfd073547e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "559d664288ff383ff8f5",
+    "url": "/checkout-counter/static/js/runtime~main.6fbfa39e.js"
+  },
+  {
+    "revision": "036466ba7477409f587a",
+    "url": "/checkout-counter/static/js/main.ba25142c.chunk.js"
+  },
+  {
+    "revision": "dc6b583134f38a043c0f",
+    "url": "/checkout-counter/static/js/2.146d931d.chunk.js"
+  },
+  {
+    "revision": "036466ba7477409f587a",
+    "url": "/checkout-counter/static/css/main.03cbc5a7.chunk.css"
+  },
+  {
+    "revision": "dc6b583134f38a043c0f",
+    "url": "/checkout-counter/static/css/2.58eec3ea.chunk.css"
+  },
+  {
+    "revision": "433d7f9261d35678975c0d2904b97414",
+    "url": "/checkout-counter/index.html"
+  }
+];
