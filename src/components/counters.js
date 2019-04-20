af6c880789dbb34ea83ff29f9ad4e2cba7aedd7f
Basil <mbonubasil@gmail.com>
2019-04-20 10:53:33 +0100

    Remove logo.svg and change counters component to a functional component

--- src/components/counters.js
+++ src/components/counters.js
@@ -1,28 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
 import Counter from "./counter";
 
-class Counters extends Component {
-  render() {
-    return (
-      <div>
-        <button
-          onClick={this.props.onReset}
-          className="btn btn-primary btn-sm m-2"
-        >
-          Reset
-        </button>
-        {this.props.counters.map(counter => (
-          <Counter
-            key={counter.id}
-            counter={counter}
-            onIncrement={this.props.onIncrement}
-            onDecrement={this.props.onDecrement}
-            onDelete={this.props.onDelete}
-          />
-        ))}
-      </div>
-    );
-  }
-}
+const Counters = ({
+  onReset,
+  counters,
+  onIncrement,
+  onDecrement,
+  onDelete
+}) => {
+  return (
+    <div>
+      <button onClick={onReset} className="btn btn-primary btn-sm m-2">
+        Reset
+      </button>
+      {counters.map(counter => (
+        <Counter
+          key={counter.id}
+          counter={counter}
+          onIncrement={onIncrement}
+          onDecrement={onDecrement}
+          onDelete={onDelete}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default Counters;
